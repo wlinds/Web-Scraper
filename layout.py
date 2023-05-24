d97fa89f427c1ec43f7fd36226324530200cd690
Canesp <philip.decanesie@gmail.com>
2023-05-24 15:31:41 +0200

    added some more layout

--- layout.py
+++ layout.py
@@ -1,11 +1,15 @@
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html
+from dash.html import Div, H1, H4, Header
+import dash_bootstrap_components as dbc
+
 
 class Layout:
-
     def __init__(self) -> None:
-
         print("loading layout")
 
     def layout(self):
-
-        return html.H1("Hello World")
+        return dbc.Container(
+            [
+                Header(H4("Web-Scraper"))
+            ], class_name= "main-container"
+        )
