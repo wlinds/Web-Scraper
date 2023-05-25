465ef1f49590eb2e11a8a460cb8cebeb549b93b1
Canesp <philip.decanesie@gmail.com>
2023-05-25 13:30:56 +0200

    move from dash to tkinter and started basic layout

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from dash import Dash, html, dcc
-import dash_bootstrap_components as dbc
-from layout import Layout
+import tkinter as tk
+from tkinter import ttk
+import sv_ttk
 
+root = tk.Tk()
+root.geometry("800x500")
 
-app = Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.FLATLY],
-    meta_tags=[dict(name="viewport", content="width=device-width, initial-scale=1.0")],
-)
+settings_frame = ttk.Frame(root, relief="solid", borderwidth=1, width= 300, height= 480, style='danger.TFrame')
+settings_frame.grid(row=0, column=0, padx=10, pady=10)
+settings_frame.grid_propagate(0)
 
-app.layout = Layout().layout()
+settings_frame_name = ttk.LabelFrame(settings_frame, text="Settings")
+settings_frame_name.grid(row=0, column=0)
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+test_entry = ttk.Entry(settings_frame)
+test_entry.grid(row= 0, column= 0, sticky="ew")
+
+sv_ttk.set_theme("dark")
+root.mainloop()
