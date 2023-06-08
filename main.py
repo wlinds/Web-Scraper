0b6c4da664c2afeb9fc9d5ef8d18d49de000e98f
Canesp <philip.decanesie@gmail.com>
2023-06-08 13:49:42 +0200

    added check for invalid URL

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 import sv_ttk
+from tkinter import filedialog
+from bs4 import BeautifulSoup
+import requests
 
 root = tk.Tk()
 root.geometry("800x500")
@@ -23,19 +26,49 @@ settings_frame.grid(row=0, column=1, padx=10, pady=10)
 settings_frame.grid_propagate(0)
 settings_frame.columnconfigure(0, weight=1)
 
-
 # if entry url is clicked.
 def on_entry_click(event):
-    if url_entry.get() == "Enter URL":  # if text == Enter URL
+
+    global entry_focused
+    entry_focused = True
+    
+    if url_entry.get() == "Enter URL" or url_entry.get() == "Invalid URL!":  # if text == Enter URL
         url_entry.delete(0, tk.END)  # Clear the entry widget
         url_entry.configure(foreground=entry_foreground)  # Change the text color
 
-
 # if not focused.
 def on_focusout(event):
-    if url_entry.get() == "":  # if text is none
+
+    if url_entry.get() == "" or "Invalid URL!":  # if text is none
         url_entry.insert(0, "Enter URL")  # Set the placeholder text
         url_entry.configure(foreground="gray")  # Change the text color
+
+def on_entry_focus_out(event):
+
+    global entry_focused
+    
+    if event.widget != url_entry and entry_focused == True:
+        entry_focused = False
+        input_text = url_entry.get()
+        print("Input:", input_text)
+        root.focus_set()
+
+        retrive_website(input_text)
+        
+
+def on_entry_return(event):
+
+    global entry_focused
+
+    input_text = url_entry.get()
+    print("Input:", input_text)
+    entry_focused = False
+    root.focus_set()
+
+    retrive_website(input_text)
+
+entry_focused = False
+entry_error = False
 
 # adding the entry to layout.
 url_entry = ttk.Entry(settings_frame, textvariable=url)
@@ -51,7 +84,37 @@ url_entry.configure(foreground="gray")
 
 # event checkers.
 url_entry.bind("<FocusIn>", on_entry_click)
-url_entry.bind("<FocusOut>", on_focusout)
+url_entry.bind("<FocusOut>", on_entry_focus_out)
+url_entry.bind("<Return>", on_entry_return)
+root.bind("<Button-1>", on_entry_focus_out)
+
+def retrive_website(URL):
+
+    global options
+    global entry_focused
+
+    try:
+        response = requests.get(URL)
+    except:
+        url_entry.delete(0, tk.END)
+        url_entry.insert(0, "Invalid URL!")
+        url_entry.configure(foreground="gray")
+
+    page = BeautifulSoup(response.content, "html.parser")
+
+    elements = page.find_all()
+
+    new_options = []
+
+    for element in elements:
+        print(f"{element.name} {element.get('class')}")
+        new_options.append(f"{element.name}, {element.get('class')}".replace("[", "").replace("]", ""))
+    
+    new_options = list(set(new_options))
+
+    options = new_options
+    
+    update_menu()
 
 # seperator for settings frame.
 divider = ttk.Separator(settings_frame, orient="horizontal")
@@ -62,7 +125,6 @@ selected_list = []
 
 # fuction for handeling selection of elements.
 def on_option_selected(*args):
-
     # removes the place holder index in option list.
     if "" in options:
         options.remove("")
@@ -72,51 +134,62 @@ def on_option_selected(*args):
 
     # checks if the selected item is in the selected list.
     if selected_item not in selected_list:
-
-        # if not appends. 
+        # if not appends.
         selected_list.append(selected_item)
 
     else:
-        
         # if is removes.
         selected_list.remove(selected_item)
 
     # resets the dropdown menu.
-    dropdown['menu'].delete(0, 'end')
-    
-    # adds back all the options to the menu. 
+    dropdown["menu"].delete(0, "end")
+
+    # adds back all the options to the menu.
     for option in options:
-        dropdown['menu'].add_command(label=option, command=tk._setit(var, option))
+        dropdown["menu"].add_command(label=option, command=tk._setit(var, option))
 
     # edits the selected options with a checkmark.
     for option in options:
         if option in selected_list:
-            dropdown["menu"].entryconfigure(option, label= "\u2713 " + option)
-    
+            dropdown["menu"].entryconfigure(option, label="\u2713 " + option)
+
     # resets the displayed option to "Select elements".
     var.set("Select elements")
     # run open_menu() after 1 millisecond.
     root.after(1, open_menu)
 
+    print(selected_list)
+
 
 # opens the option menu.
 def open_menu():
-    dropdown['state'] = 'readonly'
-    dropdown.event_generate('<Button-1>')
-    
+    dropdown["state"] = "readonly"
+    dropdown.event_generate("<Button-1>")
+
+# update menu.
+def update_menu():
+
+    # resets the dropdown menu.
+    dropdown["menu"].delete(0, "end")
+
+    # adds back all the options to the menu.
+    for option in options:
+        dropdown["menu"].add_command(label=option, command=tk._setit(var, option))
+
+
 # var for the selected option.
 var = tk.StringVar()
-options = ["", "option-01", "option-02", "option-03"] # list of options.
+options = ["", "None"]  # list of options.
 
 # creats the dropdown and position it.
 dropdown = ttk.OptionMenu(settings_frame, var, *(options), command=on_option_selected)
 dropdown.config(width=18)
-dropdown.grid(row=2, column=0, padx=0, pady=10)
+dropdown.grid(row=3, column=0, padx=0, pady=10)
 
 # sets the display option.
 var.set("Select elements")
 # runs the on_option_selected() fuction if a option is selected.
-var.trace('w', on_option_selected)
+var.trace("w", on_option_selected)
 
 sv_ttk.set_theme("dark")
 root.resizable(False, False)
